d125f4c19a552834ab8885d51e1563b307ad0db3
Fabian Lemke <info@kikkirej.net>
2017-01-19 21:08:06 +0100

    Initalisieren des Projektes

---
+++ src/main/java/net/kikkirej/stundenplanmailer/StundenplanMailer.java
@@ -0,0 +1,13 @@
+package net.kikkirej.stundenplanmailer;
+
+public class StundenplanMailer {
+
+	public static void main(String[] args) {
+		new StundenplanMailer().run(args);
+	}
+
+	private void run(String[] args) {
+		System.out.println("StundenplanMailer.run()");
+	}
+
+}
